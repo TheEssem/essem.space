5205cdec5b8ac0733e94cfc685ab94c946b40fcf
Essem <smswessem@gmail.com>
2019-07-22 13:09:05 -0500

    Update epic2.js

--- js/epic2.js
+++ js/epic2.js
@@ -1,20 +1,3 @@
-var script = document.createElement('script');
-var scripts = document.getElementsByTagName('script')[0];
-script.src = "js/libopenmpt.js";
-scripts.parentNode.insertBefore(script, scripts);
-
-function play() {
-  var libopenmpt = {};
-  libopenmpt.onRuntimeInitialized = function() {
-    var player = new ChiptuneJsPlayer(new ChiptuneJsConfig(-1));
-    player.load("images/phantasmagoria.mod", function(buffer) {
-      player.play(buffer);
-    });
-  };
-}
-
-play();
-
 particlesJS("particles-js", {
   particles: {
     number: {
